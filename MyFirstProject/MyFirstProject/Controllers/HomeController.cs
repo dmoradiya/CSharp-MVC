09d8366c3eea07638837ff16922cca6b8fc7d415
dhmec <dharmoradiya@gmail.com>
2020-11-02 12:29:07 -0700

    Receiving Get Parameters from TestPage form

--- MyFirstProject/MyFirstProject/Controllers/HomeController.cs
+++ MyFirstProject/MyFirstProject/Controllers/HomeController.cs
@@ -11,6 +11,16 @@ namespace MyFirstProject.Controllers
 {
     public class HomeController : Controller
     {
+        // Controllers are responsible for fetching and returning Views according to URIs as well as performing data manipulation via an EF context.
+
+        // A method that will return a View (whether directly or through a redirect) is referred to as an "Action", and will typically return an IActionResult.
+
+        // A data manipulation method will look more similar to our EF practice done with a console application.
+
+        // With the exception of Home/Index (which renders at the root path), pages render at /ControllerName/ActionName (Home/Privacy for this page's Privacy action). 
+        // This is also configurable if you don't like this pattern.
+
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,6 +30,7 @@ namespace MyFirstProject.Controllers
 
         public IActionResult Index()
         {
+            // "return View()" essentially means just get the view associated with this path, and send it to the client.
             return View();
         }
 
@@ -27,13 +38,15 @@ namespace MyFirstProject.Controllers
         {
             return View();
         }
+
         public IActionResult TestPage(string item)
         {
-           
+
             if (!string.IsNullOrWhiteSpace(item))
             {
                 ViewBag.AddedItem = item;
             }
+
             return View();
         }
 
@@ -42,5 +55,6 @@ namespace MyFirstProject.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
