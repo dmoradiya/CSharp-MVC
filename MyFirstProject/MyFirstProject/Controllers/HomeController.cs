1687f8b00a7a195a929cadc225cdc06fb1f1075e
dhmec <dharmoradiya@gmail.com>
2020-11-02 10:17:40 -0700

    Add TestPage and Add Link in Navbar

--- MyFirstProject/MyFirstProject/Controllers/HomeController.cs
+++ MyFirstProject/MyFirstProject/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace MyFirstProject.Controllers
         {
             return View();
         }
+        public IActionResult TestPage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
