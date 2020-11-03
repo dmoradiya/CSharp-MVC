b20d487d051e6e5916afb377b952068b423dd4c0
dhmec <dharmoradiya@gmail.com>
2020-11-03 10:33:17 -0700

    Update  EmailAddress, PersonContext  and Person class

---
+++ MyFirstProject/MyFirstProject/Models/EmailAddress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Models
+{
+    [Table("emailaddress")]
+    public class EMailAddress
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int PersonID { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
+        public string Address { get; set; }
+
+        [ForeignKey(nameof(PersonID))]
+        [InverseProperty(nameof(Models.Person.EMailAddresses))]
+        public virtual Person Person { get; set; }
+    }
+}
